48884cfff3443b744019e2096788f4cb3a8ddc11
Hamedpourhosseinjafari <hamedporhoossein@gmail.com>
2023-01-10 16:35:33 -0500

    we put the player at left corner left

--- GAME3001-W2023-labs/src/PlayScene.h
+++ GAME3001-W2023-labs/src/PlayScene.h
@@ -3,10 +3,8 @@
 #define __PLAY_SCENE__
 
 #include "Scene.h"
-#include "Plane.h"
 #include "Player.h"
-#include "Button.h"
-#include "Label.h"
+
 
 class PlayScene : public Scene
 {
@@ -24,17 +22,13 @@ private:
 	// IMGUI Function
 	void GUI_Function() const;
 	std::string m_guiTitle;
-	
+
 	glm::vec2 m_mousePosition;
 
-	Plane* m_pPlaneSprite;
 	Player* m_pPlayer;
 	bool m_playerFacingRight;
-
-	// UI Items
-	Button* m_pBackButton;
-	Button* m_pNextButton;
-	Label* m_pInstructionsLabel;
 };
+	// UI Items
+
 
 #endif /* defined (__PLAY_SCENE__) */
