412e2ffa500592c3c558d78cd8936a32d10aea71
Hamedpourhosseinjafari <hamedporhoossein@gmail.com>
2023-01-17 15:12:32 -0500

    lab2 added

---
+++ GAME3001-W2023-lab2/README.md
@@ -0,0 +1,6 @@
+#GAME3001-W2023-lab1
+lab 1 for GAME3001 in the winter 2023
+
+
+Author: Hamed Pourhossein	
+Date: January 10, 2023
---
+++ GAME3001-W2023-lab2/src/PlayScene.cpp
@@ -0,0 +1,189 @@
+#include "PlayScene.h"
+#include "Game.h"
+#include "EventManager.h"
+
+// required for IMGUI
+#include "imgui.h"
+#include "imgui_sdl.h"
+#include "Renderer.h"
+#include "Util.h"
+
+PlayScene::PlayScene()
+{
+	PlayScene::Start();
+}
+
+PlayScene::~PlayScene()
+= default;
+
+void PlayScene::Draw()
+{
+	DrawDisplayList();
+	SDL_SetRenderDrawColor(Renderer::Instance().GetRenderer(), 255, 255, 255, 255);
+}
+
+void PlayScene::Update()
+{
+	UpdateDisplayList();
+}
+
+void PlayScene::Clean()
+{
+	RemoveAllChildren();
+}
+
+void PlayScene::HandleEvents()
+{
+	EventManager::Instance().Update();
+
+	// handle player movement with GameController
+	
+
+	// handle player movement if no Game Controllers found
+	if (SDL_NumJoysticks() < 1)
+	{
+		if (EventManager::Instance().IsKeyDown(SDL_SCANCODE_A))
+		{
+			m_pPlayer->SetAnimationState(PlayerAnimationState::PLAYER_RUN_LEFT);
+			if(m_pPlayer->GetTransform()->position.x>= 20.0f )
+			{
+				m_pPlayer->GetTransform()->position = m_pPlayer->GetTransform()->position - glm::vec2(5.0f, 0.0f);
+				
+
+			}
+			m_playerFacingRight = false;
+			if(m_pPlayer->GetTransform()->position.x ==20.0f)
+			{
+				Game::Instance().ChangeSceneState(SceneState::END);
+			}
+			m_playerFacingRight = false;
+		}
+		if (EventManager::Instance().IsKeyDown(SDL_SCANCODE_D))
+		{
+			m_pPlayer->SetAnimationState(PlayerAnimationState::PLAYER_RUN_RIGHT);
+			if ( m_pPlayer->GetTransform()->position.x <= 785.0f)
+			{
+				m_pPlayer->GetTransform()->position = m_pPlayer->GetTransform()->position + glm::vec2(5.0f, 0.0f);
+			}
+			if(m_pPlayer->GetTransform()->position.x == 785.0f)
+			{
+				Game::Instance().ChangeSceneState(SceneState::END);
+	
+			}
+			m_playerFacingRight = true;
+		}
+		else
+		{
+			if (m_playerFacingRight)
+			{
+				m_pPlayer->SetAnimationState(PlayerAnimationState::PLAYER_IDLE_RIGHT);
+			}
+			else if (EventManager::Instance().IsKeyUp(SDL_SCANCODE_A))
+			{
+				m_pPlayer->SetAnimationState(PlayerAnimationState::PLAYER_IDLE_LEFT);
+			}
+		}
+	}
+	if(SDL_NumJoysticks() < 1)
+	{
+		if (EventManager::Instance().IsKeyDown(SDL_SCANCODE_W))
+		{
+			//m_pPlayer->SetAnimationState(PlayerAnimationState::PLAYER_RUN_LEFT);
+			if (m_pPlayer->GetTransform()->position.y >= 20.0f)
+			{
+				m_pPlayer->GetTransform()->position = m_pPlayer->GetTransform()->position - glm::vec2(0.0f, 5.0f);
+			}
+			if(m_pPlayer->GetTransform()->position.y == 20.0f)
+			{
+				Game::Instance().ChangeSceneState(SceneState::END);
+			}
+			m_playerFacingRight = false;
+		}
+		else if (EventManager::Instance().IsKeyDown(SDL_SCANCODE_S))
+		{
+			//m_pPlayer->SetAnimationState(PlayerAnimationState::PLAYER_RUN_RIGHT);
+			if (m_pPlayer->GetTransform()->position.y <= 575)
+			{
+				m_pPlayer->GetTransform()->position = m_pPlayer->GetTransform()->position + glm::vec2(0.0f, 5.0f);
+			}
+			if(m_pPlayer->GetTransform()->position.y == 575)
+			{
+				Game::Instance().ChangeSceneState(SceneState::END);
+			}
+
+			m_playerFacingRight = true;
+		}
+		
+	}
+
+	if (EventManager::Instance().IsKeyDown(SDL_SCANCODE_ESCAPE))
+	{
+		Game::Instance().Quit();
+	}
+
+	if (EventManager::Instance().IsKeyDown(SDL_SCANCODE_1))
+	{
+		Game::Instance().ChangeSceneState(SceneState::START);
+	}
+
+	if (EventManager::Instance().IsKeyDown(SDL_SCANCODE_2))
+	{
+		Game::Instance().ChangeSceneState(SceneState::END);
+	}
+}
+
+void PlayScene::Start()
+{
+	// Set GUI Title
+	m_guiTitle = "Play Scene";
+	
+
+
+	// Player Sprite
+	m_pPlayer = new Player();
+	m_pPlayer->GetTransform()->position = glm::vec2(50.0f, 50.0f);
+	AddChild(m_pPlayer);
+	m_playerFacingRight = true;
+
+	// Back Button
+
+
+	ImGuiWindowFrame::Instance().SetGuiFunction(std::bind(&PlayScene::GUI_Function, this));
+}
+
+void PlayScene::GUI_Function() const
+{
+	// Always open with a NewFrame
+	ImGui::NewFrame();
+
+	// See examples by uncommenting the following - also look at imgui_demo.cpp in the IMGUI filter
+	//ImGui::ShowDemoWindow();
+	
+	ImGui::Begin("GAME3001-W2023-lab1", NULL, ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_MenuBar );
+
+
+	ImGui::Separator();
+
+	 float float2[2] = { m_pPlayer->GetTransform()->position.x, m_pPlayer->GetTransform()->position.y};
+	if(ImGui::SliderFloat2("My Slider", float2, 0.0f, 800.0f))
+	{
+		if(float2[0] > m_pPlayer->GetTransform()->position.x)
+		{
+			//moving right
+
+			m_pPlayer->SetAnimationState(PlayerAnimationState::PLAYER_RUN_RIGHT);
+		}
+		if (float2[0] < m_pPlayer->GetTransform()->position.x)
+		{
+			//moving left
+
+			m_pPlayer->SetAnimationState(PlayerAnimationState::PLAYER_RUN_LEFT);
+		}
+		m_pPlayer->GetTransform()->position = glm::vec2(float2[0], float2[1]);
+
+
+
+	}
+	
+	ImGui::End();
+}
