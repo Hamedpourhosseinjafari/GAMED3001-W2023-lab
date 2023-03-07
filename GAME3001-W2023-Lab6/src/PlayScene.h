9b10787416594001b66affe826788607c6e18547
Hamedpourhosseinjafari <hamedporhoossein@gmail.com>
2023-03-07 15:41:20 -0500

    modified class starship

--- GAME3001-W2023-Lab6/src/PlayScene.h
+++ GAME3001-W2023-Lab6/src/PlayScene.h
@@ -2,14 +2,13 @@
 #ifndef __PLAY_SCENE__
 #define __PLAY_SCENE__
 
-#include "Heuristic.h"
+
 #include "Obstacle.h"
 #include "Scene.h"
 #include "StarShip.h"
 #include "Target.h"
-#include "Tile.h"
-#include <queue>
-#include "TileComparator.h"
+
+
 
 class PlayScene : public Scene
 {
@@ -35,69 +34,18 @@ private:
 
 	// Debugging Variables
 	bool m_bDebugView;
-
-	// Pathfinding Objects and Functions
-	std::vector<Tile*> m_pGrid;
 	bool m_isGridEnabled;
 
 	void m_buildGrid();
-	[[nodiscard]] bool m_getGridEnabled() const;
-	void m_setGridEnabled(bool state);
-	void m_computeTileCosts();
+
 
 	// obstacle list
 	std::vector<Obstacle*> m_pObstacles;
 	void m_buildObstacles();
 
-	//obstacle utility functions
-	void m_removeObstacleAt(int col, int row);
-	void m_removeObstacleAt(glm::vec2 grid_position);
-	void m_removeAllObstacles();
-
-	// collections , variables for pathfinding 
-	std::priority_queue<Tile*,std::vector<Tile*>,TileComparator > m_pOpenList;//visited but not expanded list of tiles
-	std::list<Tile*> m_pPathList;
-	bool m_pathFound = false;
-	
-
-
-	// path finding functions
-	void m_findShortestPath();
-	void m_buildPathList();
-	void m_displayPathList() const;
-	void m_resetPathFinding();
-	void m_resetSimulation();
-
-
-	//move function
-	void m_moveObj();
 
 
 
-
-	// convenience Functions to convert world to grid space
-	Tile* m_getTile(int col, int row) const;
-	Tile* m_getTile(glm::vec2 grid_position) const;
-
-	template <typename T>
-	void M_addObjectToGrid(T*& object, int col, int row, TileStatus status);
-
-	template <typename T>
-	void m_moveGameObject(T*& object, const  int col, const  int row, const TileStatus status);
-	
-
-	// heuristic
-	Heuristic  m_currentHeuristic;
-
-	//tile map variables and functions
-	std::string m_tileMap;
-	void m_initializeTileMap();
-	void m_buildTileMap();
-
-	//convenience functions to update the tile map
-
-	void m_updateTileMap(int col, int row, TileStatus status);
-	void m_updateTileMap(glm::vec2 grid_position, TileStatus status);
 
 };
 
